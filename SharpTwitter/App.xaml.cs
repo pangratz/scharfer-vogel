0d00aa72833bb93efe3fa1bb44b9ca6e84235220
pangratz666 <pangratz666@gmail.com>
2010-11-19 19:48:02 +0100

    initial import

---
+++ SharpTwitter/App.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Xml;
+using System.Net;
+using Twitterizer;
+using System.Configuration;
+
+namespace SharpTwitter
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private TwitterCommunicator twitterComm;
+
+        public App() {
+            Console.WriteLine("Tsting the construct");
+
+            twitterComm = new TwitterCommunicator();
+        }
+
+        public void Tweet(string message) {
+            twitterComm.Tweet(message);
+        }
+
+    }
+
+    public class TwitterCommunicator
+    {
+        private OAuthTokens tokens;
+
+        public TwitterCommunicator()
+        {
+            tokens = new OAuthTokens();
+            tokens.AccessToken = TwitterAccountConstants.oAuthToken;
+            tokens.AccessTokenSecret = TwitterAccountConstants.oAuthTokenSecret;
+            tokens.ConsumerKey = TwitterAccountConstants.oAuthConsumerKey;
+            tokens.ConsumerSecret = TwitterAccountConstants.oAuthConsumerSecret;
+
+            TwitterResponse<TwitterStatusCollection> timelineResponse = TwitterTimeline.HomeTimeline(tokens);
+            if (timelineResponse.Result == RequestResult.Success)
+            {
+                // show the timeline
+                TwitterStatusCollection statusCollection = timelineResponse.ResponseObject as TwitterStatusCollection;
+                foreach (TwitterStatus status in statusCollection)
+                {
+                    // Console.WriteLine("{0} wrote {1}", status.User.ScreenName, status.Text);
+                }
+            }
+            else
+            {
+                // excption
+            }
+
+            TwitterResponse<TwitterUser> resp = TwitterUser.Show(tokens, "pangratz");
+            TwitterUser twitterUser = resp.ResponseObject as TwitterUser;
+            string desc = twitterUser.Description;
+            Console.WriteLine("description: {0}", desc);
+        }
+
+        public void Tweet(string message)
+        {
+            Console.WriteLine("Sending Tweet: {0}", message);
+            TwitterResponse<TwitterStatus> statusResponse = TwitterStatus.Update(tokens, message);
+            if (statusResponse.Result == RequestResult.Success)
+            {
+                Console.WriteLine("Successfull Tweet");
+            }
+            else
+            {
+                Console.WriteLine("error: {0}", statusResponse.ErrorMessage);
+            }
+        }
+    }
+
+}
---
+++ SharpTwitter/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SharpTwitter
+{
+    /// <summary>
+    /// Interaktionslogik für TwitterWindow.xaml
+    /// </summary>
+    public partial class TwitterWindow : Window
+    {
+
+        private Brush defaultForeground;
+
+        public TwitterWindow()
+        {
+            InitializeComponent();
+            defaultForeground = charactersLeftLabel.Foreground;
+        }
+
+        private void tweetMessageChanged(object sender, TextChangedEventArgs e)
+        {
+            int length = tweetTextBox.Text.Length;
+            int charsLeft = (140 - length);
+            if (charsLeft < 0) {
+                charactersLeftLabel.Foreground = Brushes.Red;
+            } else {
+                charactersLeftLabel.Foreground = defaultForeground;
+            }
+            charactersLeftLabel.Content = (140 - length);
+
+        }
+
+        private void keyPressed(object sender, KeyEventArgs e)
+        {
+            // check if the user wants to submit the tweet
+            if (e.Key == Key.Enter) {
+                // set this enter press to handled
+                e.Handled = true;
+
+                string tweetMessage = tweetTextBox.Text;
+                if (tweetMessage.Length == 0 || tweetMessage.Length > 140) {
+                    // invalid tweet length
+                    return;
+                }
+
+                // tweet the entered message
+                App tweetApp = (App) App.Current;
+                tweetApp.Tweet(tweetMessage);
+
+                tweetTextBox.Clear();
+            }
+            else if (e.Key == Key.Escape) {
+                e.Handled = true;
+                tweetTextBox.Clear();
+            }
+        }
+    }
+}
